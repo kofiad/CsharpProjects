3e54747ff9f4b5657d74f2667c87dbce5d164f28
adaambiigabz <adaambiikgabz45@gmail.com>
2025-01-08 11:57:51 +0000

    Add subscription expiration notification logic to BusinessRules

---
+++ BusinessRules/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage;
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription expires soon.  Renew now!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration}days!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires in 1 day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired");
+}
