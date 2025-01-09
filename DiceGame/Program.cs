dc9e5bd7c43927509af3345f9ba1f88f3bae9924
kofiad <kofiadaba@gmail.com>
2025-01-09 20:27:18 +0000

    Refactor dice rolling logic and improve code formatting in Program.cs

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -3,22 +3,22 @@ Random dice = new Random();
 int roll1 = dice.Next();
 int roll2 = dice.Next(101);
 int roll3 = dice.Next(50,101);
+
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
 
 
-int firstValue = 500; 
+int firstValue = 500;
 int secondValue = 600;
 int largerValue = Math.Max(firstValue, secondValue);
-Console.WriteLine(largerValue); 
+
+Console.WriteLine(largerValue);
 
 Random newDice = new Random();
-
 int newRoll1 = newDice.Next(1, 7);
 int newRoll2 = newDice.Next(1, 7);
 int newRoll3 = newDice.Next(1, 7);
-
 int total = newRoll1 + newRoll2 + newRoll3;
 
 Console.WriteLine($"Dice roll: {newRoll1} + {newRoll2} + {newRoll3} = {total}");
