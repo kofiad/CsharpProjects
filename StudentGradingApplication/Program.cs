af5fa3ba54e7042a89261737caded0f50eb38426
kofiad <kofiadaba@gmail.com>
2025-01-09 21:47:30 +0000

    Implement grade calculation logic and add application description in Program.cs

--- StudentGradingApplication/Program.cs
+++ StudentGradingApplication/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+This application is a simple console application that automates the calculation of grades for each student in a class.
+The application reads the student's name, the number of exams, and each exam score.
+The application calculates the average exam score and assigns a letter grade based on the average.
+The application then displays the student's name, average exam score, and letter grade.
+*/
+
+Random random = new();
+
+int[,] studentScores = {
+     { random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100) },
+     { random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100) },
+     { random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100) },
+     { random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100), random.Next(0, 100) }
+     };
+int average = 0;
+int sum = 0;
+
+foreach (int score in studentScores)
+{
+    sum += score;
+    average = sum / studentScores.Length;
+}
