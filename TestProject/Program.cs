30fab4f3a480c839b9d6adbd5bf37ee1574f3c90
kofiad <kofiadaba@gmail.com>
2025-01-11 21:15:47 +0000

    Add solution file and update Program.cs with comparison examples

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue1 = " a ";
+string myValue2 = "A";
+Console.WriteLine( myValue1.Trim().ToLower() == myValue2.Trim().ToLower());
